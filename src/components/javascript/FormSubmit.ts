15a068f0a175a104bc7806f86eb5a9b9e3ffa426
jrenning <jackalexrenning@gmail.com>
2022-08-18 16:46:28 -0500

    Added initial project sumbission commands for all of the languages.

--- src/components/javascript/FormSubmit.ts
+++ src/components/javascript/FormSubmit.ts
@@ -8,18 +8,7 @@ type Javascript = z.infer<typeof formSchema>
 
 export const JavascriptSubmit = async (data: Javascript) => {
 
-    const command: any = await new Command("cmd", ["/C","cod", "-n", "."], {
-      cwd: "C:\\Projects\\Flutter\\learning_dart",
-    })
-      .execute(
-      )
-      .catch(function (err) {
-        console.log(err);
-      });;
-
-    if (command["stderr"]) {
-        alert("VSCode CLI wasn't found, make sure you can run the code command in your terminal")
-    }
+    
 
     
 
