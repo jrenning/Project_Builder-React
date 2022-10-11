39c4584b9fb8f4b158d48d5195a786413c36a3f5
jrenning <jackalexrenning@gmail.com>
2022-10-11 09:52:53 -0500

    Added way to select a template, still need to add data for them.

--- src/components/python/Step1Form.tsx
+++ src/components/python/Step1Form.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, ChangeEvent } from "react";
 import { z } from "zod";
 import { Form, useForm } from "../form_components/Form";
 import { Input } from "../form_components/Input";
@@ -6,11 +6,13 @@ import { SelectBox } from "../shared/SelectBox";
 import SubmitButton from "../form_components/SubmitButton";
 import { PythonFormState } from "./PythonForm";
 import { useForm as useHookForm } from "react-hook-form";
+import FormButton from "../shared/FormButton";
 
 export const formSchema1 = z.object({
   Project_Name: z.string().min(1, "Please enter a name"),
   Project_Type: z.enum(["New Project", "Existing Template"]),
   Path: z.string(),
+  Template: z.string().optional(),
 });
 
 type Step1Data = z.infer<typeof formSchema1>;
@@ -26,27 +28,43 @@ function Step1Form({ formData, setFormState, setFormStep }: Props) {
     schema: formSchema1,
   });
 
+  // whether or not a template should be asked for
+  const [templateEnter, setTemplateEnter] = useState(false);
 
   const Step1Submit = (data: Step1Data) => {
-    setFormState((prevState) => ({
-      ...prevState,
-      ...data,
-    }));
-    setFormStep(1);
-}
+    // if template wasn't selected update state and form step
+    if (!data.Template) {
+      setFormState((prevState) => ({
+        ...prevState,
+        ...data,
+      }));
+      setFormStep(1);
+    }
 
+    // if template is chosen create project
+    // TODO, call create project (template version?)
+  };
 
+  const checkTemplate = (data: ChangeEvent<HTMLSelectElement>) => {
+    if (data.target.value == "Use Existing Template") {
+      setTemplateEnter(true);
+    } else {
+      setTemplateEnter(false);
+    }
+  };
 
+  const openFileSelection = () => {
+    // TODO add file selection
+  }
 
-useEffect(()=> {
-    console.log('here')
-    console.log(formData)
+  // keep form data in case you click back
+  useEffect(() => {
     form.reset({
-        Project_Name: formData.Project_Name,
-        Path: formData.Path,
-        Project_Type: formData.Project_Type
-    })
-},[])
+      Project_Name: formData.Project_Name,
+      Path: formData.Path,
+      Project_Type: formData.Project_Type,
+    });
+  }, []);
 
   return (
     <Form onSubmit={Step1Submit} form={form}>
@@ -56,19 +74,33 @@ useEffect(()=> {
         placeholder="Project Name"
         {...form.register("Project_Name")}
       />
+      <FormButton name="Choose Path" onClick={openFileSelection}/>
       <Input
         label="Path"
         type="text"
         placeholder="Path"
-        {...form.register("Path")}
+        {...form.register("Path", {disabled: true})}
       />
       <SelectBox
         default_option="New Project"
         options={["Use Existing Template"]}
         select_name="Type of Project"
         {...form.register("Project_Type")}
+        onChange={checkTemplate}
       />
-      <SubmitButton name="Next Step" />
+      {templateEnter && (
+        <SelectBox
+          default_option=""
+          options={["Filler Template", "Filer Template"]}
+          select_name="Template"
+          {...form.register("Template")}
+        />
+      )}
+      {templateEnter ? (
+        <SubmitButton name="Create Project" />
+      ) : (
+        <SubmitButton name="Next Step" />
+      )}
     </Form>
   );
 }
